4065d32db5cbc870823be628dacddf1ffb85dbee
Tim Maher <tim@timmaher.dev>
2020-01-07 11:26:19 -0500

    Projecting and binding to components

---
+++ 03-projecting-components/app/auth-form/auth-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+import { User } from './auth-form.interface';
+
+@Component({
+  selector: 'auth-form',
+  templateUrl: './auth-form.component.html',
+})
+export class AuthFormComponent {
+  @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
+
+  onSubmit(value: User) {
+    this.submitted.emit(value);
+  }
+}
