fb224907c4b2fc2ec1bf5a4986e9cb94552e9165
Tim Maher <tim@timmaher.dev>
2020-01-08 10:03:50 -0500

    Dynamic components with ComponentFactoryResolver

---
+++ 11-dynamic-components/app/app.component.ts
@@ -0,0 +1,37 @@
+import {
+  Component,
+  ViewChild,
+  ViewContainerRef,
+  ComponentFactoryResolver,
+  AfterContentInit,
+} from '@angular/core';
+
+import { AuthFormComponent } from './auth-form/auth-form.component';
+
+import { User } from './auth-form/auth-form.interface';
+
+type BetterViewContainerRef = ViewContainerRef | null;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements AfterContentInit {
+  @ViewChild('entry', { read: ViewContainerRef })
+  entry: BetterViewContainerRef = null;
+
+  constructor(private resolver: ComponentFactoryResolver) {}
+
+  loginUser(user: User) {
+    console.log('Login', user);
+  }
+
+  ngAfterContentInit() {
+    const authFormFactory = this.resolver.resolveComponentFactory(
+      AuthFormComponent,
+    );
+    if (this.entry) {
+      this.entry.createComponent(authFormFactory);
+    }
+  }
+}
