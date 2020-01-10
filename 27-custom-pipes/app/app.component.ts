058b77dcebb6786db383eac155d0dcdf9336a811
Tim Maher <tim@timmaher.dev>
2020-01-10 11:05:06 -0500

    Creating a custom pipe

---
+++ 27-custom-pipes/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+interface File {
+  name: string;
+  size: number;
+  type: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  files: File[] = [];
+
+  ngOnInit() {
+    this.files = [
+      { name: 'logo.svg', size: 2120109, type: 'image/svg' },
+      { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
+      { name: 'background.png', size: 1784562, type: 'image/png' },
+    ];
+  }
+}
