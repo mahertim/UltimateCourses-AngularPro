7481a2f02a5dfb2b3adc997d4b6668599913cf3b
Tim Maher <tim@timmaher.dev>
2020-01-10 10:51:12 -0500

    Creating a custom structural Directive

---
+++ 26-custom-structural-directive/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  items = [
+    {
+      name: 'Mark Hoppus',
+      age: 44,
+      location: 'California',
+    },
+    {
+      name: 'Tom Delonge',
+      age: 41,
+      location: 'California',
+    },
+    {
+      name: 'Travis Barker',
+      age: 41,
+      location: 'California',
+    },
+  ];
+  constructor() {
+    setTimeout(() => {
+      this.items = [
+        ...this.items,
+        { name: 'Matt Skiba', age: 40, location: 'California' },
+      ];
+    }, 2000);
+  }
+}
