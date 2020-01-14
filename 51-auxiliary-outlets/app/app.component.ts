f731fe000d69b29e68190554332408cfd92c467b
Tim Maher <tim@timmaher.dev>
2020-01-14 12:31:13 -0500

    Auxiliary named router outlets

---
+++ 51-auxiliary-outlets/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import 'rxjs/add/operator/filter';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .subscribe((event) => console.log(event));
+  }
+}
