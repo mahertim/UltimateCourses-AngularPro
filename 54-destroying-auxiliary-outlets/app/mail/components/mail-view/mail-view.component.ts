0752a904a37705a41394f4758907a03aa3a41f4b
Tim Maher <tim@timmaher.dev>
2020-01-14 13:11:31 -0500

    Destroying auxiliary outlets

---
+++ 54-destroying-auxiliary-outlets/app/mail/components/mail-view/mail-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mail-view',
+  styleUrls: ['./mail-view.component.scss'],
+  templateUrl: './mail-view.component.html',
+})
+export class MailViewComponent {}
