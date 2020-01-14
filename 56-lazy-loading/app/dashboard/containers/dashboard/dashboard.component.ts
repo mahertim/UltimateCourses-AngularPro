be84cc5d6c0ddad0dabe059bebed263c9dac69dc
Tim Maher <tim@timmaher.dev>
2020-01-14 15:01:44 -0500

    Lazy-loading modules

---
+++ 56-lazy-loading/app/dashboard/containers/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-app',
+  styleUrls: ['dashboard.component.scss'],
+  templateUrl: 'dashboard.component.html',
+})
+export class DashboardComponent {}
