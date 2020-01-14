8f30f57d44c93d0e497e6ab62b992e2d1c074269
Tim Maher <tim@timmaher.dev>
2020-01-14 15:20:36 -0500

    Custom preloading strategies

---
+++ 58-custom-preloading-strategy/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import {
+  RouterModule,
+  Routes,
+  PreloadingStrategy,
+  Route,
+} from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+import { MailModule } from './mail/mail.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+import { AppComponent } from './app.component';
+
+export class CustomPreload implements PreloadingStrategy {
+  preload(route: Route, fn: () => Observable<any>): Observable<any> {
+    return route.data && (route.data as { preload: boolean }).preload
+      ? fn()
+      : Observable.of(null);
+  }
+}
+
+export const ROUTES: Routes = [
+  {
+    path: 'dashboard',
+    data: { preload: true },
+    loadChildren: () => DashboardModule,
+  },
+  { path: '**', redirectTo: 'mail/folder/inbox' },
+];
+
+@NgModule({
+  declarations: [AppComponent],
+  providers: [CustomPreload],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    MailModule,
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: CustomPreload }),
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
